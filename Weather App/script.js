a21e1bbeba54cd0c6f0cbc03feedd2c135186814
Chetna <cmaske580@gmail.com>
2024-05-20 14:21:13 +0530

    Completed

--- Weather App/script.js
+++ Weather App/script.js
@@ -11,5 +11,43 @@ let city = document.getElementById("city");
     let jsonData =await getData.json();
     console.log(jsonData);
     console.log(jsonData.name);
+
+    if(jsonData.cod == 400){
+        alert("Please Enter Location")
+        image.src="error1.png";
+        city.innerHTML="";
+        temp.innerHTML="";
+        type.innerHTML="";
+    }
+    if(jsonData.cod == 404){
+        alert("Please Enter Write Location")
+        image.src="error2.png";
+        city.innerHTML=search;
+        temp.innerHTML="";
+        type.innerHTML="";
+    }
+    city.innerHTML=search;
+    temp.innerHTML=Math.floor(jsonData.main.temp)+"°C";
+    type.innerHTML=jsonData.weather[0].main;
+
+    if(type.innerHTML == "Clouds"){
+        image.src="Clouds.png"
+    }else if(type.innerHTML == "Clear"){
+        image.src="clears.png"
+    }else if(type.innerHTML == "Rain"){
+        image.src="rain.png"
+    }else if(type.innerHTML == "Snow"){
+        image.src="rain.png"
+    }else if(type.innerHTML == "Haze"){
+        image.src="haze.png"
+    }else if(type.innerHTML == "Strom"){
+        image.src="strom.png"
+    }
+    input.value=""
+ }
+
+ function myFun(){
+    search=input.value;
+    data(search)
  }
  
